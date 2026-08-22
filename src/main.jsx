159702817f9c02bb24484ff07e33dc9290414110
Suvojit1203 <debsuvojit12@gmail.com>
2026-08-23 03:10:23 +0530

    updating wedding website files

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import './gallery.css';
 import './custom.css';
 import './celebration-motion.css';
 import './gallery-celebration.css';
+import './mobile-polish.css';
 
 createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>);
